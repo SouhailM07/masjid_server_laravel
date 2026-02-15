1d420a5b884402ae63c8be1ae7d92f11922a613e
souhail <lightshadow416@gmail.com>
2026-02-15 19:35:29 +0200

    project core was built

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/hi',function(){
+    return response()->json(["message"=>"welcome to the absolute shadow"],201);
+});

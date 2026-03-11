5dc2644ee371950689274199d2a494cc939256a4
souhail <lightshadow416@gmail.com>
2026-03-12 01:58:28 +0200

    config : configured telescope properly

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -16,7 +16,10 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     public function register(): void
     {
         // Telescope::night();
-
+        if ($this->app->environment('local') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
         $this->hideSensitiveRequestDetails();
 
         $isLocal = $this->app->environment('local');
